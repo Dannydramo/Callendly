89c14f4a10a101def4b17dc522a2ee8ec37dbe5e
Dannydramo <alumandudaniel217@gmail.com>
2023-03-30 11:44:31 -0700

    added env

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,11 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDvV_sI9poT2bInZEC5xcEdGNueAuj9JYA",
-  authDomain: "callendly-authentication.firebaseapp.com",
-  projectId: "callendly-authentication",
-  storageBucket: "callendly-authentication.appspot.com",
-  messagingSenderId: "155182495395",
-  appId: "1:155182495395:web:f77e97ee99c4b4cebdb116",
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,12 +1,16 @@
 import React, { useContext } from "react";
-import { Navigate } from "react-router-dom";
+
 import AuthContext from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useContext(AuthContext);
 
   if (!user) {
-    return <p>You must be authenticated to access this page.</p>;
+    return (
+      <p className="mt-24 text-6xl">
+        You must be authenticated to access this page.
+      </p>
+    );
   }
 
   return children;
